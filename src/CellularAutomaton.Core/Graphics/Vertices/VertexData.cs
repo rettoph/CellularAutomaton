99861c520683e0409d654173a2523752c6adf159
rettoph <anthony@turner.ec>
2024-03-04 16:28:26 -0700

    Start working on more vertex structs

---
+++ src/CellularAutomaton.Core/Graphics/Vertices/VertexData.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System.Runtime.InteropServices;
+
+namespace CellularAutomaton.Core.Graphics.Vertices
+{
+    [StructLayout(LayoutKind.Explicit)]
+    public struct VertexData : IVertexType
+    {
+        public static uint ColorMask = 0x00ffffff;
+        public static uint AsleepMask = 0x80000000;
+
+        private static readonly VertexDeclaration VertexDeclaration = new VertexDeclaration
+        (
+            new VertexElement(0, VertexElementFormat.Single, VertexElementUsage.TextureCoordinate, 1)
+        );
+
+        VertexDeclaration IVertexType.VertexDeclaration => VertexDeclaration;
+
+        [FieldOffset(0)]
+        public int Index;
+
+        [FieldOffset(4)]
+        private uint _data;
+
+        public uint Color
+        {
+            get => _data & ColorMask;
+            set => _data = value & ColorMask;
+        }
+
+        public bool Asleep
+        {
+            set
+            {
+                if (value)
+                {
+                    _data |= AsleepMask;
+                }
+                else
+                {
+                    _data &= ~AsleepMask;
+                }
+            }
+        }
+    }
+}
---
+++ src/CellularAutomaton.Core/Graphics/Vertices/VertexIndex.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System.Runtime.InteropServices;
+
+namespace CellularAutomaton.Core.Graphics.Vertices
+{
+    [StructLayout(LayoutKind.Explicit)]
+    public struct VertexIndex : IVertexType
+    {
+        private static readonly VertexDeclaration VertexDeclaration = new VertexDeclaration
+        (
+            new VertexElement(0, VertexElementFormat.Single, VertexElementUsage.TextureCoordinate, 0)
+        );
+
+        VertexDeclaration IVertexType.VertexDeclaration => VertexDeclaration;
+
+        [FieldOffset(0)]
+        public int Index;
+    }
+}
